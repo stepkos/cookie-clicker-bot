baaf635e27981a10679f984da3c2953cc64d0b8f
stepkos <stepkos@wp.pl>
2021-04-06 21:39:38 +0200

    buy buildings

--- cookieClickerBot.py
+++ cookieClickerBot.py
@@ -5,19 +5,25 @@ import time
 class CookieClickerBot:
     def __init__(self):
         self.driver = webdriver.Chrome(Settings.PATH)
-        self.driver.get("https://orteil.dashnet.org/cookieclicker/")
+        self.driver.get(Settings.URL)
         self.driver.implicitly_wait(5)
 
         self.cookie = self.driver.find_element_by_id('bigCookie')
         self.cookie_counter = self.driver.find_element_by_id('cookies')
-        self.buildings = [self.driver.find_elements_by_id('productPrice' + str(i))[0] for i in range(17, -1, -1)]
+        self.clickCookie = lambda times: [self.cookie.click() for i in range(times)]
+        self.main()
 
+    def main(self):
+        while True:
+            self.clickCookie(1000)
 
-        time.sleep(20)
+            buildings = self.driver.find_elements_by_css_selector('.product.unlocked.enabled')
+            [[building.click() for i in range(5)] for building in buildings[::-1]]
 
-        [print(i.text) for i in self.buildings]
-
-
+            # upgrades = self.driver.find_elements_by_css_selector('.crate.upgrade.enabled')
+            # if upgrades:
+                # upgrades[-1].click()
+            # [upgrade.click() for upgrade in upgrades[::-1]]
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 class Settings:
     PATH = r"C:\Program Files (x86)\chromedriver.exe"
+    URL = r"https://orteil.dashnet.org/cookieclicker/"
