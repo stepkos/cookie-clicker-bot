804c09050a667820adae7b396d005e25cec9a43d
stepkos <stepkos@wp.pl>
2021-04-07 11:30:59 +0200

    print hour when exception

--- cookieClickerBot.py
+++ cookieClickerBot.py
@@ -1,14 +1,19 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from time import sleep
+from datetime import datetime
 
-PATH = r"C:\Program Files (x86)\chromedriver.exe"
+DRIVER_PATH = r"C:\Program Files (x86)\chromedriver.exe"
+SAVE_PATH = r""
 URL = r"https://orteil.dashnet.org/cookieclicker/"
 
 class CookieClickerBot:
     def __init__(self):
-        self.driver = webdriver.Chrome(PATH)
+        # options = Options()
+        # options.page_load_strategy = 'eager'
+        # self.driver = Chrome(DRIVER_PATH, options=options)
+        self.driver = Chrome(DRIVER_PATH)
         self.driver.get(URL)
-        # self.driver.implicitly_wait(5)
         sleep(5)
 
         self.clickCookie = lambda times: [self.driver.find_element_by_id('bigCookie').click() for i in range(times)]
@@ -28,18 +33,20 @@ class CookieClickerBot:
             self.driver.find_element_by_id('upgrade1').click()
             self.driver.find_element_by_id('upgrade0').click()
         except:
-            print('clickFirstUpgrade exception')
+            now = datetime.now()
+            print('clickUpgrades exception - {}:{:02d}:{:02d}'.format(now.hour, now.minute, now.second))
 
     def clickBuildings(self):
         try:
             buildings = self.driver.find_elements_by_css_selector('.product.unlocked.enabled')
             [[building.click() for i in range(10)] for building in buildings[::-1]]
         except:
-            print('clickBuildings exception')
+            now = datetime.now()
+            print('clickBuildings exception - {}:{:02d}:{:02d}'.format(now.hour, now.minute, now.second))
 
     def main(self):
         while True:
-            self.clickCookie(3000)
+            self.clickCookie(300)
             self.clickUpgrades()
             self.clickBuildings()
 
