107e8616d5e19b5cec7aeaeb89e07532011f9ce0
stepkos <stepkos@wp.pl>
2021-04-07 11:50:59 +0200

    message static method

--- cookieClickerBot.py
+++ cookieClickerBot.py
@@ -7,6 +7,7 @@ DRIVER_PATH = r"C:\Program Files (x86)\chromedriver.exe"
 SAVE_PATH = r""
 URL = r"https://orteil.dashnet.org/cookieclicker/"
 
+
 class CookieClickerBot:
     def __init__(self):
         # options = Options()
@@ -19,18 +20,28 @@ class CookieClickerBot:
         self.clickCookie = lambda times: [self.driver.find_element_by_id('bigCookie').click() for i in range(times)]
 
         input('Press enter to start ')
+        CookieClickerBot.message('CookieClickerBot is starting!')
         self.main()
+
 
     @staticmethod
     def getTime():
         now = datetime.now()
         return '{}:{:02d}:{:02d}'.format(now.hour, now.minute, now.second)
 
+
+    @staticmethod
+    def message(content):
+        print(CookieClickerBot.getTime() + ' - ' + content)
+
+
     def importSave(self):
         pass
 
+
     def exportSave(self):
         pass
+
 
     def clickUpgrades(self):
         try:
@@ -38,20 +49,23 @@ class CookieClickerBot:
             self.driver.find_element_by_id('upgrade1').click()
             self.driver.find_element_by_id('upgrade0').click()
         except:
-            print('clickUpgrades exception - ' + CookieClickerBot.getTime())
+            CookieClickerBot.message('clickUpgrades method raise an exception')
+
 
     def clickBuildings(self):
         try:
             buildings = self.driver.find_elements_by_css_selector('.product.unlocked.enabled')
             [[building.click() for i in range(10)] for building in buildings[::-1]]
         except:
-            print('clickBuildings exception - ' + CookieClickerBot.getTime())
+            CookieClickerBot.message('clickBuildings method raise an exception')
+
 
     def main(self):
         while True:
-            self.clickCookie(3000)
+            self.clickCookie(300)
             self.clickUpgrades()
             self.clickBuildings()
+
 
 
 if __name__ == '__main__':
